317e1ba4380b608effe790bc5e2d1030bb7bae73
earshariff <earshariff@gmail.com>
2015-04-13 00:31:40 +0530

    Comments added test commit

--- App2/MainPage.xaml.cs
+++ App2/MainPage.xaml.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+/test comment
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
 namespace App2
